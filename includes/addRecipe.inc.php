8d491669e3782273de17c01e353909352cf8f76c
RusuI <79109790+RusuI@users.noreply.github.com>
2021-06-08 15:19:45 +0300

    Update addRecipe.inc.php

--- includes/addRecipe.inc.php
+++ includes/addRecipe.inc.php
@@ -1,27 +1,34 @@
 <?php
-
-if (isset($_POST["submit"])) {
-
-    require_once 'database.php';
-    $name = $_POST["name"];
-    $description = $_POST["description"];
-    $prep = $_POST["email"];
-    $cook = $_FILES["file"];
-    $diff = $_FILES["file"]["size"];
-    
-
-
-
     require_once 'database.php';
     require_once 'recipeFunctions.php';
-
-}
 
 $data = file_get_contents('php://input');
 $data=json_decode($data,true);
 
-echo json_encode($data["list"]);
+// echo json_encode($data["list"]);
+// echo json_encode($data["listStep"]);*/
 
-?>
+// $response["list"]=$data["list"];
+// $response["listStep"]=$data["listStep"];
+// $response["name"] = $data["name"];
+// $response["description"] = $data["description"];
+// $response["prep_time"] = $data["prep_time"];
+// $response["cook_time"] = $data["cook_time"];
+// $response["serv"] = $data["serv"];
+// $response["diff"] = $data["diff"];
+if(emptyInputRecipe($data["list"],$data["listStep"],$data["name"],$data["description"],$data["prep_time"],$data["cook_time"],$data["serv"],$data["diff"])){
+    http_response_code();
+}
+
+
+//http_response_code(400);
+
+//echo json_encode($response);
+
+
+
+
+
+
 
 
