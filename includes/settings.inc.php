5747ea86c66af9c6512c8b679dd2e395c867e2d0
Cristina Murariu <75524144+cristinaaa272@users.noreply.github.com>
2021-06-10 12:37:18 +0300

    updated some settings

--- includes/settings.inc.php
+++ includes/settings.inc.php
@@ -3,36 +3,39 @@
 if (isset($_POST["submit"])) {
 
     require_once 'database.php';
+    require_once 'userFunctions.php';
     $name = $_POST["name"];
     $description = $_POST["description"];
     $email = $_POST["email"];
     $post_image = $_FILES["file"]['name']; 
     $post_image_temp = $_FILES["file"]['tmp_name']; 
+    $user = $_SESSION['id'];
 
     move_uploaded_file($post_image_temp, "../imagesLogin/$post_image");
-
-
-
-    require_once 'userFunctions.php';
-
-    if (emptyInputSettings($name, $email, $description) !== false) { //if it's anything besides false, just throw an error
+    if (emptyInputSettings($name, $email, $description) !== false) { 
 
         header("location: ../views/settings.php?error=emptyinput");
         exit();
     }
 
-    $result = update_settings($connection, $name, $email, $description,$post_image);
+    $result = update_settings($connection, $name, $email, $description);
+    
     if ($result == 1) {
+        $_SESSION['username'] = $name;
         if($post_image != null){
-            $query = "UPDATE users SET ";
-            $query .= "image = '{$post_image}' ";
-            $query .= "WHERE id = '{$_SESSION['id']}'";
-            
-            $addImage = mysqli_query($connection, $query);
-
-            if($addImage){
+            $sql = "UPDATE users SET image=? WHERE id=?;";
+            $stmt = mysqli_stmt_init($connection);
+            if (!mysqli_stmt_prepare($stmt, $sql)) {
+                header("location: ../views/settings.php?error=updateFailed");
+                exit();
+            }
+            mysqli_stmt_bind_param($stmt, "ss", $post_image, $user);
+            mysqli_stmt_execute($stmt);
+            if($stmt){
                 header("location: ../views/profile.php");
             }
+            mysqli_stmt_close($stmt);
+
         }else{
             header("location: ../views/profile.php");
         }
--- includes/userFunctions.php
+++ includes/userFunctions.php
@@ -239,32 +239,25 @@ function emptyInputSettings($username, $email, $description)
 }
 
 
-function update_settings($connection, $name, $email, $description, $imgContent)
+
+function update_settings($connection, $name, $email, $description)
 {
-    $name   = mysqli_real_escape_string($connection, $name);
-    $email   = mysqli_real_escape_string($connection, $email);
-    $description   = mysqli_real_escape_string($connection, $description);
-    $imgContent   = mysqli_real_escape_string($connection, $imgContent);
-
-
     $user_id = $_SESSION['id'];
-    $query = "UPDATE users SET ";
-    $query .= "username = '{$name}', ";
-    $query .= "email = '{$email}', ";
-    $query .= "description = '{$description}' ";
-    $query .= "WHERE id = '{$user_id}'";
-    $update_user = mysqli_query($connection, $query);
-
-
-    if (!$update_user) {
-        die("Query failed" . mysqli_error($connection));
-    } else {
-        $_SESSION['username'] = $name;
-    }
     $result = 1;
-    return $result;
-}
 
+    $sql = "UPDATE users SET username=?,email=?,description=? WHERE id=?;";
+    $stmt = mysqli_stmt_init($connection);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        $result = 0;
+        return $result;
+    }
+
+    mysqli_stmt_bind_param($stmt, "ssss", $name, $email, $description, $user_id);
+    mysqli_stmt_execute($stmt);
+    return $result;
+
+    mysqli_stmt_close($stmt);
+}
 
 /* END OF SETTINGS FUNCTIONS*/
 
@@ -314,8 +307,8 @@ function getFavoritesByUid($connection, $user)
     $user = $_SESSION['id'];
     $sql = "SELECT id,name,description,author_id FROM recipe JOIN favorites ON favorites.recipe_id=recipe.id WHERE user_id=?;";
 
-    $stmt = mysqli_stmt_init($connection);
 
+    $stmt = mysqli_stmt_init($connection);
     mysqli_stmt_prepare($stmt, $sql);
     mysqli_stmt_bind_param($stmt, "s", $user);
     mysqli_stmt_execute($stmt);
@@ -324,10 +317,30 @@ function getFavoritesByUid($connection, $user)
     $result_data = mysqli_stmt_get_result($stmt);
     return $result_data;
     mysqli_stmt_close($stmt);
-
-    header("location: ../views/favorites.php?error=none");
-    exit();
 }
 
+
+
+
+function getFavoritesPhotos($connection, $recipe)
+{
+    $sql = "SELECT photo FROM recipes_pictures JOIN favorites ON favorites.recipe_id = recipes_pictures.recipe_id WHERE recipe_id = ? LIMIT 1;";
+    $stmt = mysqli_stmt_init($connection);
+
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        $result = 0;
+        return $result;
+    }
+    debug_to_console($recipe);
+    debug_to_console($sql);
+
+    mysqli_stmt_bind_param($stmt, "s", $recipe);
+    mysqli_stmt_execute($stmt);
+
+    $result_data = mysqli_stmt_get_result($stmt);
+
+    return $result_data;
+    mysqli_stmt_close($stmt);
+}
 
 /*END OF FAVORITES FUNCTION */
